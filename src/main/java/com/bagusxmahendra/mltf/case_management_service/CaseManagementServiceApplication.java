9f1f6b735be7cc21901eb870f00f49815c161c16
Bagus <bagusmwicaksono@gmail.com>
2026-08-27 10:44:49 +0000

    Initialize

---
+++ src/main/java/com/bagusxmahendra/mltf/case_management_service/CaseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bagusxmahendra.mltf.case_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseManagementServiceApplication.class, args);
+	}
+
+}
